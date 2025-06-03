eeded8ebf351453fa97a71785160747202f1c192
GujjulaShravya27 <gujjulashravya1@gmail.com>
2025-06-03 19:22:53 +0530

    Add files via upload

---
+++ UnaryDec.java
@@ -0,0 +1,13 @@
+public class UnaryDec
+{
+ public static void main(String args[])
+
+{
+int num1=10;
+//pre decrement
+int num2=--num1;
+//post decrement
+int num3=num1--;
+System.out.println("num1="+num1+"num2="+num2+"num3="+num3);
+} 
+   }
