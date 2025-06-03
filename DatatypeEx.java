8d5de8158f138bc59b586af05239225f73b12a76
GujjulaShravya27 <gujjulashravya1@gmail.com>
2025-06-03 15:05:57 +0530

    Add files via upload

---
+++ DatatypeEx.java
@@ -0,0 +1,26 @@
+//program using primitive datatypes in java
+
+public class DatatypeEx
+{
+  public static void main(String args[])
+  {
+ 
+//declaring variables
+char ch='a';
+Boolean value=true;
+byte num1=100;
+short num2=150;
+int num3=1300;
+long num4=129000l;
+float num5=3.14f;
+double num6=321.09876;
+//print values
+System.out.println(ch+","+value);
+System.out.println("integer type:"+num1+","+num2+","+num3+","+num4);
+System.out.println("floating type:"+num5+","+num6);
+   }
+}
+   
+
+
+
