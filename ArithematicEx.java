4b82f6795c20f3dd94ef31bf8d416c96bf06dd63
GujjulaShravya27 <gujjulashravya1@gmail.com>
2025-06-03 13:18:39 +0530

    Add files via upload

---
+++ ArithematicEx.java
@@ -0,0 +1,15 @@
+public class ArithematicEx
+{
+public static void main(String args[])
+{
+int num1=10;
+int num2=20;
+
+System.out.println("sum="+(num1+num2));
+System.out.println("sub="+(num1-num2));
+System.out.println("mul="+(num1*num2));
+System.out.println("div="+(num1/num2));
+System.out.println("mod="+(num1%num2));
+
+}
+}
