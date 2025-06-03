9ce9e0668e3bafd81ff3da9a1780a4b57979f14e
GujjulaShravya27 <gujjulashravya1@gmail.com>
2025-06-03 19:16:59 +0530

    Add files via upload

---
+++ RelationalOp.java
@@ -0,0 +1,14 @@
+public class RelationalOp
+{
+public static void main(String args[])
+{
+int num1=20;
+int num2=20;
+System.out.println("num==num2:"+(num1==num2));
+System.out.println("num>num2:"+(num1>num2));
+System.out.println("num<num2:"+(num1<num2));
+System.out.println("num>=num2:"+(num1>=num2));
+System.out.println("num<=num2:"+(num1<=num2));
+System.out.println("num!=num2:"+(num1!=num2));
+}
+}
