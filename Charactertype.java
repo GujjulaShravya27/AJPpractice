83bb6dc4d91392aa7a0e7c9be8e46608f16b528d
GujjulaShravya27 <gujjulashravya1@gmail.com>
2025-06-11 15:22:01 +0530

    Add files via upload

---
+++ Charactertype.java
@@ -0,0 +1,17 @@
+package Com.Shravya;
+
+public class Charactertype {
+
+	public static void main(String[] args) {
+		char Ch='s';
+		if(Ch=='a'||Ch=='i'||Ch=='o'||Ch=='e'||Ch=='o')
+			System.out.println(Ch+"is vowel");
+		else
+			System.out.println(Ch+",is consonant");
+			
+		
+		
+
+	}
+
+}
