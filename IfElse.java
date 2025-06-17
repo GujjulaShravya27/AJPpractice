0e47f7bfb64982a567c123138c10cc0b97c88999
GujjulaShravya27 <gujjulashravya1@gmail.com>
2025-06-17 12:39:20 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,19 @@
+package Com.shravya;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		int num =30;
+		if(num%2==0) 
+			System.out.println(num+"is even number");
+		else
+				System.out.println(num+"is odd number");
+		}
+
+	
+
+		// TODO Auto-generated method stub
+
+	}
+
+
